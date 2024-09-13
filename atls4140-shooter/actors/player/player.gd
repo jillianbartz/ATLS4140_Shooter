e2bd25ccf343684aefb72dbe597d05df52531be0
Jillian Bartz <jillianbartz@econ1-130-227-edu.int.colorado.edu>
2024-09-13 11:40:42 -0600

    bare add of new features

--- atls4140-shooter/actors/player/player.gd
+++ atls4140-shooter/actors/player/player.gd
@@ -3,18 +3,35 @@ extends CharacterBody2D
 @export var projectile_scene: Resource
 @export var move_speed: float = 200.0
 
-var timeToaddDestination: float
+var hold: bool = false
+var holdAmount: float = 0.0
+var sprint: bool = false
 
 func _input(event):
 	if (event is InputEventMouseButton):
 		# Only shoot on left click pressed down
 		if (event.button_index == 1 and event.is_pressed()):
+			holdAmount = 0
+			hold = true
 			var new_projectile = projectile_scene.instantiate()
 			get_parent().add_child(new_projectile)
-			
 			var projectile_forward = Vector2.from_angle(rotation)
 			new_projectile.fire(projectile_forward, 1000.0)
 			new_projectile.position = $ProjectileRefPoint.global_position
+		if(event.is_released()):
+			hold = false
+
+func _process(delta):
+	if(hold == true):
+		holdAmount += delta
+		if(holdAmount >= .6):
+			await get_tree().create_timer(.3).timeout
+			var new_projectile = projectile_scene.instantiate()
+			get_parent().add_child(new_projectile)
+			var projectile_forward = Vector2.from_angle(rotation)
+			new_projectile.fire(projectile_forward, 3000.0)
+			new_projectile.position = $ProjectileRefPoint.global_position
+			hold = false
 
 func _physics_process(delta):
 	look_at(get_viewport().get_mouse_position())
@@ -23,8 +40,19 @@ func _physics_process(delta):
 		"move_right", \
 		"move_up", \
 		"move_down") * move_speed
+	var newSprint = velocity
+	if(sprint):
+		velocity = newSprint * 2
+	else:
+		velocity = newSprint
 	move_and_slide()
 
-func _process(delta):
-	if(Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
-		print("press")
+
+func _on_progress_bar_stamina_empty(value: Variant) -> void:
+	if(!value):
+		print("false")
+		sprint = false
+	
+	if(value):
+		sprint = true
+		print("true")
---
+++ atls4140-shooter/progress_bar.gd
@@ -0,0 +1,35 @@
+extends ProgressBar
+
+var dash: bool = false
+var dashAmount: float = 0.0
+var canRegen: bool = false
+
+signal stamina_empty(value)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if(Input.is_action_just_pressed("dash")):
+		dash = true
+	if(Input.is_action_just_released("dash")):
+		dash = false
+	if(dash):
+		if(value <= 0):
+			stamina_empty.emit(false)
+			await get_tree().create_timer(5).timeout
+			canRegen = true
+		else:
+			canRegen = false
+			value -= delta
+			stamina_empty.emit(true)
+	elif(canRegen):
+		if(value <= max_value):
+			value += delta
+		else:
+			canRegen = false
+	else:
+		stamina_empty.emit(false)
